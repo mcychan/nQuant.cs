5fa6259f20afa303acd69f7ed0aa573f82414f6f
Miller Cy Chan <miller.chan@gmail.com>
2018-10-24 18:08:23 +0800

    Add files via upload

--- nQuant.Master/Properties/AssemblyInfo.cs
+++ nQuant.Master/Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nQuant")]
+[assembly: AssemblyTitle("nQuant.c#")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nQuant")]
-[assembly: AssemblyCopyright("Copyright Matt Wrock 2011")]
+[assembly: AssemblyProduct("nQuant.c#")]
+[assembly: AssemblyCopyright("Copyright © Miller Cy Chan 2018")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyTrademarkAttribute("23:7f111f3b95fb43d5856c50710dc8e958a7fbbca0")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyTrademarkAttribute("ImgV64 ®")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
