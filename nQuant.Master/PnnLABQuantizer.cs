af1041387ce5e13b773ea33d10d4125e113b0765
Miller Cy Chan <miller.chan@gmail.com>
2021-02-17 09:59:13 +0800

    Add files via upload

--- nQuant.Master/PnnLABQuantizer.cs
+++ nQuant.Master/PnnLABQuantizer.cs
@@ -8,7 +8,7 @@ namespace PnnQuant
 {
     public class PnnLABQuantizer : PnnQuantizer
     {
-        private double PR = .2126, PG = .7152, PB = .0722;
+        private double PR = .299, PG = .587, PB = .114;
         private double ratio = 1.0;
         private Dictionary<int, CIELABConvertor.Lab> pixelMap = new Dictionary<int, CIELABConvertor.Lab>();
         private sealed class Pnnbin
