bd7a6ae590ba4035c2cc546df44122dcc25d4c21
Miller Cy Chan <miller.chan@gmail.com>
2022-01-10 16:23:10 +0800

    Update PnnLABQuantizer.cs

--- nQuant.Master/PnnLABQuantizer.cs
+++ nQuant.Master/PnnLABQuantizer.cs
@@ -450,7 +450,7 @@ namespace PnnQuant
         protected override int[] Dither(int[] pixels, Color[] palettes, int nMaxColors, int width, int height, bool dither)
         {
             int[] qPixels;
-	    dither &= nMaxColors <= palette.Length;
+	    dither &= nMaxColors <= palettes.Length;
             if (nMaxColors <= 32 || (hasSemiTransparency && weight < .3))
                 qPixels = GilbertCurve.Dither(width, height, pixels, palettes, this, 1.5f);
             else
