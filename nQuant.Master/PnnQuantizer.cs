29e7e232da8ed6cecc19861c522372d5575253ea
Miller Cy Chan <miller.chan@gmail.com>
2022-08-27 16:12:13 +0800

    Update PnnQuantizer.cs

--- nQuant.Master/PnnQuantizer.cs
+++ nQuant.Master/PnnQuantizer.cs
@@ -174,7 +174,7 @@ namespace PnnQuant
             var weight = nMaxColors * 1.0 / maxbins;
             if (weight > .003 && weight < .005)
                 quan_rt = 0;
-            if (weight < .025 && PG < 1) {
+            if (weight < .025 && nMaxColors > 32) {
                 var delta = 3 * (.025 + weight);
                 PG -= delta;
                 PB += delta;
