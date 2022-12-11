ec80c668f77eda71ccf2e2f223580522691681d9
Miller Cy Chan <miller.chan@gmail.com>
2022-12-11 13:05:11 +0800

    Add files via upload

--- nQuant.Master/Properties/AssemblyInfo.cs
+++ nQuant.Master/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nQuant.c#")]
+[assembly: AssemblyTitle("nQuant.cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nQuant.c#")]
+[assembly: AssemblyProduct("nQuant.cs")]
 [assembly: AssemblyCopyright("Copyright © Miller Cy Chan 2018 - 2022")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
