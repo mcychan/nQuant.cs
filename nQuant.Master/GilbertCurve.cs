2c0a4ffa7cf12c1bbc7ee2852624181a39e6fecd
Miller Cy Chan <miller.chan@gmail.com>
2023-04-27 17:30:10 +0800

    Update GilbertCurve.cs

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -127,8 +127,8 @@ namespace nQuant.Master
 			var yDiff = diffuse ? 1 : CIELABConvertor.Y_Diff(c1, c2);
 
 			var errLength = denoise ? error.Length - 1 : 0;
-			var offset = Math.Floor((1 - yDiff) * 3);
-			var ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.Sqr(Math.Sqrt(DITHER_MAX) + offset) : DITHER_MAX;
+			var edge = Math.Floor((1 - yDiff) * 3);
+			var ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.Sqr(Math.Sqrt(DITHER_MAX) + edge) : DITHER_MAX;
 			for (int j = 0; j < errLength; ++j)
 			{
 				if (Math.Abs(error[j]) >= ditherMax)
