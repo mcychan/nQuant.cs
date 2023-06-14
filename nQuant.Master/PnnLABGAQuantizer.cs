f5d42ab4d89866c91a9ecbf5c3ae562414db037e
Miller Cy Chan <miller.chan@gmail.com>
2023-06-14 09:38:33 +0800

    Update PnnLABGAQuantizer.cs

--- nQuant.Master/PnnLABGAQuantizer.cs
+++ nQuant.Master/PnnLABGAQuantizer.cs
@@ -50,7 +50,7 @@ namespace PnnQuant
 			m_pixels = m_pq.GrabPixels(source, _nMaxColors, ref hasSemiTransparency);
 			minRatio = (hasSemiTransparency || nMaxColors < 64) ? 0 : .85;
 			maxRatio = Math.Min(1.0, nMaxColors / ((nMaxColors < 64) ? 500.0 : 50.0));
-			_dp = maxRatio < .1 ? 100 : 10;
+			_dp = maxRatio < .1 ? 500 : 10;
 		}
 
 		private PnnLABGAQuantizer(PnnLABQuantizer pq, int[] pixels, int bitmapWidth, int nMaxColors)
