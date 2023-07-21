8f3a874afef1cde0c4c5e18fc2e659c96bcaee83
Miller Cy Chan <miller.chan@gmail.com>
2023-07-21 22:35:22 +0800

    Add files via upload

--- nQuant.Master/PnnLABGAQuantizer.cs
+++ nQuant.Master/PnnLABGAQuantizer.cs
@@ -12,286 +12,305 @@ Copyright (c) 2023 Miller Cy Chan
 
 namespace PnnQuant
 {
-    public class PnnLABGAQuantizer : Chromosome<PnnLABGAQuantizer>, IDisposable
-    {
-        // boolean variable to ensure dispose
-        // method executes only once
-        private bool disposedValue;
+	public class PnnLABGAQuantizer : Chromosome<PnnLABGAQuantizer>, IDisposable
+	{
+		// boolean variable to ensure dispose
+		// method executes only once
+		private bool disposedValue;
 
-        private double _fitness = double.NegativeInfinity;
-        public double ratioX = 0, ratioY = 0;
-        private double[] _objectives;
-        private PnnLABQuantizer m_pq;
-        public double[] ConvertedObjectives { get; private set; }
-        public double[] Objectives { get => _objectives; }
+		private double _fitness = double.NegativeInfinity;
+		public double ratioX = 0, ratioY = 0;
+		private double[] _objectives;
+		private PnnLABQuantizer m_pq;
+		public double[] ConvertedObjectives { get; private set; }
+		public double[] Objectives { get => _objectives; }
 
-        private readonly Random _random;
+		private readonly Random _random;
 
-        private static int _dp = 1, _nMaxColors = 256;
-        private static double minRatio = 0, maxRatio = 1.0;
-        private static int[] m_pixels;
-        private static int _bitmapWidth;
+		private static int _dp = 1, _nMaxColors = 256;
+		private static double minRatio = 0, maxRatio = 1.0;
+		private static int[] m_pixels;
+		private static int _bitmapWidth;
 
-        private static readonly ConcurrentDictionary<string, double[]> _fitnessMap = new();
+		private static readonly ConcurrentDictionary<string, double[]> _fitnessMap = new();
 
-        public PnnLABGAQuantizer(PnnLABQuantizer pq, Bitmap source, int nMaxColors)
-        {
-            // increment value when criteria violation occurs
-            _objectives = new double[4];
-            _bitmapWidth = source.Width;
-            _random = new Random(_bitmapWidth * source.Height);
+		public PnnLABGAQuantizer(PnnLABQuantizer pq, Bitmap source, int nMaxColors)
+		{
+			// increment value when criteria violation occurs
+			_objectives = new double[4];
+			_bitmapWidth = source.Width;
+			_random = new Random(_bitmapWidth * source.Height);
 
-            m_pq = new PnnLABQuantizer(pq);
-            if (pq.IsGA)
-                return;
+			m_pq = new PnnLABQuantizer(pq);
+			if (pq.IsGA)
+				return;
 
-            _nMaxColors = nMaxColors;
+			_nMaxColors = nMaxColors;
 
-            var hasSemiTransparency = false;
-            m_pixels = m_pq.GrabPixels(source, _nMaxColors, ref hasSemiTransparency);
-            minRatio = (hasSemiTransparency || nMaxColors < 64) ? .01 : .85;
-            maxRatio = Math.Min(1.0, nMaxColors / ((nMaxColors < 64) ? 500.0 : 50.0));
-            _dp = maxRatio < .1 ? 10000 : 100;
-        }
+			var hasSemiTransparency = false;
+			m_pixels = m_pq.GrabPixels(source, _nMaxColors, ref hasSemiTransparency);
+			minRatio = (hasSemiTransparency || nMaxColors < 64) ? .0111 : .85;
+			maxRatio = Math.Min(1.0, nMaxColors / ((nMaxColors < 64) ? 500.0 : 50.0));
+			_dp = maxRatio < .1 ? 10000 : 100;
+		}
 
-        private PnnLABGAQuantizer(PnnLABQuantizer pq, int[] pixels, int bitmapWidth, int nMaxColors)
-        {
-            m_pq = new PnnLABQuantizer(pq);
-            m_pixels = pixels;
-            _bitmapWidth = bitmapWidth;
-            _random = new Random(pixels.Length);
-            _nMaxColors = nMaxColors;
-        }
+		private PnnLABGAQuantizer(PnnLABQuantizer pq, int[] pixels, int bitmapWidth, int nMaxColors)
+		{
+			m_pq = new PnnLABQuantizer(pq);
+			m_pixels = pixels;
+			_bitmapWidth = bitmapWidth;
+			_random = new Random(pixels.Length);
+			_nMaxColors = nMaxColors;
+		}
 
-        private string RatioKey
-        {
-            get
-            {
-                var sb = new StringBuilder();
-                sb.Append((int) (ratioX * _dp));
-                var difference = Math.Abs(ratioX - ratioY);
-                if (difference <= 0.0000001)
-                    return sb.ToString();
+		private string RatioKey
+		{
+			get
+			{
+				var sb = new StringBuilder();
+				sb.Append((int) (ratioX * _dp));
+				var difference = Math.Abs(ratioX - ratioY);
+				if (difference <= 0.0000001)
+					return sb.ToString();
 
-                sb.Append(";").Append((int) (ratioY * _dp * 100));
-                return sb.ToString();
-            }
-        }
+				sb.Append(";").Append((int) (ratioY * _dp * 100));
+				return sb.ToString();
+			}
+		}
+		
+		protected delegate double AmplifyFn(double val);
+		private AmplifyFn GetAmplifyFn(bool tooSmall)
+		{
+			if (tooSmall)
+				return val => Math.Exp(val);
+			return val => Math.Log(val);
+		}
 
-        private void CalculateError(double[] errors)
-        {
-            var maxError = maxRatio < .1 ? 1.0 : .125;
-            double fitness = 0;
-            for (int i = 0; i < errors.Length; ++i)
-            {
-                errors[i] /= maxError * m_pixels.Length;
-                if (i == 0 && errors[i] > maxError)
-                    errors[i] *= errors[i];
-                else if (errors[i] > 2 * maxError)
-                    errors[i] *= errors[i];
-                fitness -= errors[i];
-            }
+		private void CalculateError(double[] errors)
+		{
+			var maxError = maxRatio < .1 ? .5 : .0625;
+			if(m_pq.HasAlpha)
+				maxError = 1;
 
-            _objectives = errors;
-            _fitness = fitness;
-        }
+			double fitness = 0;
+			bool tooSmall = false; // any error < exp(1.0) concluded as too small
+			for (int i = 0; i < errors.Length; ++i)
+			{
+				errors[i] /= maxError * m_pixels.Length;
+				if(errors[i] < 3)
+					tooSmall = true;
+			}
+
+			var amplifyFn = GetAmplifyFn(tooSmall);
+			for (int i = 0; i < errors.Length; ++i)
+			{
+				if (i == 0 && errors[i] > maxError)
+					errors[i] *= amplifyFn(errors[i]);
+				else if (errors[i] > 2 * maxError)
+					errors[i] *= amplifyFn(errors[i]);
+				fitness -= errors[i];
+			}
+
+			_objectives = errors;
+			_fitness = fitness;
+		}
 
 
-        private void CalculateFitness()
-        {
-            var ratioKey = RatioKey;
-            if (_fitnessMap.TryGetValue(ratioKey, out _objectives))
-            {
-                _fitness = -1f * _objectives.Sum();
-                return;
-            }
+		private void CalculateFitness()
+		{
+			var ratioKey = RatioKey;
+			if (_fitnessMap.TryGetValue(ratioKey, out _objectives))
+			{
+				_fitness = -1f * _objectives.Sum();
+				return;
+			}
 
-            _objectives = new double[4];
-            m_pq.SetRatio(ratioX, ratioY);
-            var palette = new Color[_nMaxColors];
-            m_pq.Pnnquan(m_pixels, ref palette, ref _nMaxColors);
+			_objectives = new double[4];
+			m_pq.SetRatio(ratioX, ratioY);
+			var palette = new Color[_nMaxColors];
+			m_pq.Pnnquan(m_pixels, ref palette, ref _nMaxColors);
 
-            int threshold = maxRatio < .1 ? -64 : -112;
-            var errors = new double[_objectives.Length];
-            for (int i = 0; i < m_pixels.Length; ++i)
-            {
-                if (BlueNoise.RAW_BLUE_NOISE[i & 4095] > threshold)
-                    continue;
+			int threshold = maxRatio < .1 ? -64 : -112;
+			var errors = new double[_objectives.Length];
+			for (int i = 0; i < m_pixels.Length; ++i)
+			{
+				if (BlueNoise.RAW_BLUE_NOISE[i & 4095] > threshold)
+					continue;
 
-                m_pq.GetLab(m_pixels[i], out CIELABConvertor.Lab lab1);
-                var qPixelIndex = m_pq.NearestColorIndex(palette, m_pixels[i], i);
-                m_pq.GetLab(palette[qPixelIndex].ToArgb(), out CIELABConvertor.Lab lab2);
+				m_pq.GetLab(m_pixels[i], out CIELABConvertor.Lab lab1);
+				var qPixelIndex = m_pq.NearestColorIndex(palette, m_pixels[i], i);
+				m_pq.GetLab(palette[qPixelIndex].ToArgb(), out CIELABConvertor.Lab lab2);
 
-                if (m_pq.HasAlpha)
-                {
-                    errors[0] += BitmapUtilities.Sqr(lab2.L - lab1.L);
-                    errors[1] += BitmapUtilities.Sqr(lab2.A - lab1.A);
-                    errors[2] += BitmapUtilities.Sqr(lab2.B - lab1.B);
-                    errors[3] += BitmapUtilities.Sqr(lab2.alpha - lab1.alpha) / Math.Exp(1.5);
-                }
-                else
-                {
-                    errors[0] += Math.Abs(lab2.L - lab1.L);
-                    errors[1] += Math.Sqrt(BitmapUtilities.Sqr(lab2.A - lab1.A) + BitmapUtilities.Sqr(lab2.B - lab1.B));
-                }
-            }
-            CalculateError(errors);
-            _fitnessMap[ratioKey] = _objectives;
-        }
+				if (m_pq.HasAlpha)
+				{
+					errors[0] += BitmapUtilities.Sqr(lab2.L - lab1.L);
+					errors[1] += BitmapUtilities.Sqr(lab2.A - lab1.A);
+					errors[2] += BitmapUtilities.Sqr(lab2.B - lab1.B);
+					errors[3] += BitmapUtilities.Sqr(lab2.alpha - lab1.alpha) / Math.Exp(1.5);
+				}
+				else
+				{
+					errors[0] += Math.Abs(lab2.L - lab1.L);
+					errors[1] += Math.Sqrt(BitmapUtilities.Sqr(lab2.A - lab1.A) + BitmapUtilities.Sqr(lab2.B - lab1.B));
+				}
+			}
+			CalculateError(errors);
+			_fitnessMap[ratioKey] = _objectives;
+		}
 
-        public Bitmap QuantizeImage(bool dither)
-        {
-            m_pq.SetRatio(ratioX, ratioY);
-            var palette = new Color[_nMaxColors];
-            m_pq.Pnnquan(m_pixels, ref palette, ref _nMaxColors);
-            return m_pq.QuantizeImage(m_pixels, _bitmapWidth, _nMaxColors, dither);
-        }
+		public Bitmap QuantizeImage(bool dither)
+		{
+			m_pq.SetRatio(ratioX, ratioY);
+			var palette = new Color[_nMaxColors];
+			m_pq.Pnnquan(m_pixels, ref palette, ref _nMaxColors);
+			return m_pq.QuantizeImage(m_pixels, _bitmapWidth, _nMaxColors, dither);
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            // check if already disposed
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // free managed objects here
-                    m_pixels = null;
-                    _fitnessMap.Clear();
-                }
+		protected virtual void Dispose(bool disposing)
+		{
+			// check if already disposed
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// free managed objects here
+					m_pixels = null;
+					_fitnessMap.Clear();
+				}
 
-                // free unmanaged objects here
+				// free unmanaged objects here
 
-                // set the bool value to true
-                disposedValue = true;
-            }
-        }
+				// set the bool value to true
+				disposedValue = true;
+			}
+		}
 
-        // The consumer object can call
-        // the below dispose method
-        public void Dispose()
-        {
-            // Invoke the above virtual
-            // dispose(bool disposing) method
-            Dispose(disposing: true);
+		// The consumer object can call
+		// the below dispose method
+		public void Dispose()
+		{
+			// Invoke the above virtual
+			// dispose(bool disposing) method
+			Dispose(disposing: true);
 
-            // Notify the garbage collector
-            // about the cleaning event
-            GC.SuppressFinalize(this);
-        }
+			// Notify the garbage collector
+			// about the cleaning event
+			GC.SuppressFinalize(this);
+		}
 
-        private double Randrange(double min, double max)
-        {
-            return min + _random.NextDouble() * (max - min);
-        }
+		private double Randrange(double min, double max)
+		{
+			return min + _random.NextDouble() * (max - min);
+		}
 
-        public double[] Ratios
-        {
-            get => new double[] { ratioX, ratioY };
-        }
+		public double[] Ratios
+		{
+			get => new double[] { ratioX, ratioY };
+		}
 
-        public void SetRatio(double ratioX, double ratioY)
-        {
+		public void SetRatio(double ratioX, double ratioY)
+		{
 			var difference = Math.Abs(ratioX - ratioY);
 			if (difference <= minRatio)
 				ratioY = ratioX;
-            this.ratioX = Math.Min(Math.Max(ratioX, minRatio), maxRatio);
-            this.ratioY = Math.Min(Math.Max(ratioY, minRatio), maxRatio);
-        }
+			this.ratioX = Math.Min(Math.Max(ratioX, minRatio), maxRatio);
+			this.ratioY = Math.Min(Math.Max(ratioY, minRatio), maxRatio);
+		}
 
-        public float Fitness
-        {
-            get => (float) _fitness;
-        }
+		public float Fitness
+		{
+			get => (float) _fitness;
+		}
 		
-        private double RotateLeft(double u, double v, double delta) {
-            var theta = Math.PI * Randrange(minRatio, maxRatio) / Math.Exp(delta);
-            var result = u * Math.Sin(theta) + v * Math.Cos(theta);
-            if(result <= minRatio || result >= maxRatio)
-                result = RotateLeft(u, v, delta + .5);
-            return result;
-        }
+		private double RotateLeft(double u, double v, double delta) {
+			var theta = Math.PI * Randrange(minRatio, maxRatio) / Math.Exp(delta);
+			var result = u * Math.Sin(theta) + v * Math.Cos(theta);
+			if(result <= minRatio || result >= maxRatio)
+				result = RotateLeft(u, v, delta + .5);
+			return result;
+		}
 		
-        private double RotateRight(double u, double v, double delta) {
-            var theta = Math.PI * Randrange(minRatio, maxRatio) / Math.Exp(delta);
-            var result = u * Math.Cos(theta) - v * Math.Sin(theta);
-            if(result <= minRatio || result >= maxRatio)
-                result = RotateRight(u, v, delta + .5);
-            return result;
-        }
+		private double RotateRight(double u, double v, double delta) {
+			var theta = Math.PI * Randrange(minRatio, maxRatio) / Math.Exp(delta);
+			var result = u * Math.Cos(theta) - v * Math.Sin(theta);
+			if(result <= minRatio || result >= maxRatio)
+				result = RotateRight(u, v, delta + .5);
+			return result;
+		}
 
-        public PnnLABGAQuantizer Crossover(PnnLABGAQuantizer mother, int numberOfCrossoverPoints, float crossoverProbability)
-        {
-            var child = MakeNewFromPrototype();
-            if (_random.Next(100) <= crossoverProbability)
-                return child;
+		public PnnLABGAQuantizer Crossover(PnnLABGAQuantizer mother, int numberOfCrossoverPoints, float crossoverProbability)
+		{
+			var child = MakeNewFromPrototype();
+			if (_random.Next(100) <= crossoverProbability)
+				return child;
 
-            var ratioX = RotateRight(this.ratioX, mother.Ratios[1], 0.0);
-            var ratioY = RotateLeft(this.ratioY, mother.Ratios[0], 0.0);
-            child.SetRatio(ratioX, ratioY);
-            child.CalculateFitness();
-            return child;
-        }
+			var ratioX = RotateRight(this.ratioX, mother.Ratios[1], 0.0);
+			var ratioY = RotateLeft(this.ratioY, mother.Ratios[0], 0.0);
+			child.SetRatio(ratioX, ratioY);
+			child.CalculateFitness();
+			return child;
+		}
 
-        private double BoxMuller(double value) {
-            var r1 = Randrange(minRatio, maxRatio);
-            return Math.Sqrt(-2 * Math.Log(value)) * Math.Cos(2 * Math.PI * r1);
-        }
+		private double BoxMuller(double value) {
+			var r1 = Randrange(minRatio, maxRatio);
+			return Math.Sqrt(-2 * Math.Log(value)) * Math.Cos(2 * Math.PI * r1);
+		}
 
-        public void Mutation(int mutationSize, float mutationProbability)
-        {
-            // check probability of mutation operation
-            if (_random.Next(100) > mutationProbability)
-                return;
+		public void Mutation(int mutationSize, float mutationProbability)
+		{
+			// check probability of mutation operation
+			if (_random.Next(100) > mutationProbability)
+				return;
 
-            var ratioX = this.ratioX;
-            var ratioY = this.ratioY;
-            if (_random.NextDouble() > .5)
-                ratioX = BoxMuller(ratioX);
-            else
-                ratioY = BoxMuller(ratioY);
+			var ratioX = this.ratioX;
+			var ratioY = this.ratioY;
+			if (_random.NextDouble() > .5)
+				ratioX = BoxMuller(ratioX);
+			else
+				ratioY = BoxMuller(ratioY);
 
-            SetRatio(ratioX, ratioY);
-            CalculateFitness();
-        }
+			SetRatio(ratioX, ratioY);
+			CalculateFitness();
+		}
 
-        public void ResizeConvertedObjectives(int numObj)
-        {
-            ConvertedObjectives = new double[numObj];
-        }
+		public void ResizeConvertedObjectives(int numObj)
+		{
+			ConvertedObjectives = new double[numObj];
+		}
 
-        public PnnLABGAQuantizer MakeNewFromPrototype()
-        {
-            var child = new PnnLABGAQuantizer(m_pq, m_pixels, _bitmapWidth, _nMaxColors);
-            var minRatio2 = 2 * minRatio;
-            if (minRatio2 > 1)
-                minRatio2 = 0;
-            var ratioX = Randrange(minRatio, maxRatio);
-            var ratioY = ratioX < minRatio2 ? Randrange(minRatio, maxRatio) : ratioX;
-            child.SetRatio(ratioX, ratioY);
-            child.CalculateFitness();
-            return child;
-        }
+		public PnnLABGAQuantizer MakeNewFromPrototype()
+		{
+			var child = new PnnLABGAQuantizer(m_pq, m_pixels, _bitmapWidth, _nMaxColors);
+			var minRatio2 = 2 * minRatio;
+			if (minRatio2 > 1)
+				minRatio2 = 0;
+			var ratioX = Randrange(minRatio, maxRatio);
+			var ratioY = ratioX < minRatio2 ? Randrange(minRatio, maxRatio) : ratioX;
+			child.SetRatio(ratioX, ratioY);
+			child.CalculateFitness();
+			return child;
+		}
 
-        public Random Random
-        {
-            get => _random;
-        }
+		public Random Random
+		{
+			get => _random;
+		}
 
-        public String Result
-        {
-            get
-            {
-                var difference = Math.Abs(ratioX - ratioY);
-                if (difference <= 0.0000001)
-                    return ratioX.ToString("0.######");
-                return ratioX.ToString("0.######") + ", " + ratioY.ToString("0.######");
-            }
-        }
+		public String Result
+		{
+			get
+			{
+				var difference = Math.Abs(ratioX - ratioY);
+				if (difference <= 0.0000001)
+					return ratioX.ToString("0.######");
+				return ratioX.ToString("0.######") + ", " + ratioY.ToString("0.######");
+			}
+		}
 
-        public static int MaxColors
-        {
-            get => _nMaxColors;
-        }
+		public static int MaxColors
+		{
+			get => _nMaxColors;
+		}
 
-    }
+	}
 }
