106a3cc605f921d90711ab35abf1bf9897f57fb1
Miller Cy Chan <miller.chan@gmail.com>
2023-11-14 20:11:51 +0800

    Update GilbertCurve.cs

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -79,7 +79,7 @@ namespace nQuant.Master
 			ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.Sqr(Math.Sqrt(DITHER_MAX) + edge) : DITHER_MAX;
 			if (palette.Length / weight > 5000 && (weight > .045 || (weight > .01 && palette.Length <= 64)))
 				ditherMax = (byte) BitmapUtilities.Sqr(5 + edge);
-			else if (palette.Length / weight < 3200 && palette.Length > 16 && palette.Length < 256)
+			else if (palette.Length / weight < 3200 && palette.Length >= 16 && palette.Length < 256)
 				ditherMax = (byte) BitmapUtilities.Sqr(5 + edge);
 			thresold = DITHER_MAX > 9 ? -112 : -64;
 			weights = new float[0];
