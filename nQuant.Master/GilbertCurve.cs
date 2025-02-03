3a47ffb842a1c3c1133f4b02bd25616a320617ca
Miller Cy Chan <miller.chan@gmail.com>
2025-02-03 21:57:29 +0800

    Add files via upload

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -114,7 +114,7 @@ namespace nQuant.Master
 			int a_pix = (int)Math.Min(Byte.MaxValue, Math.Max(error[3], 0.0));
 
 			Color c2 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
-			if (saliencies != null && (palette.Length < 3|| margin > 6))
+			if (saliencies != null)
 			{
 				var strength = 1 / 3f;
 				var beta = palette.Length > 8 ? .7f : 1;
@@ -124,11 +124,15 @@ namespace nQuant.Master
 				else if (palette.Length <= 8 || CIELABConvertor.Y_Diff(pixel, c2) < (2 * acceptedDiff))
 					c2 = BlueNoise.Diffuse(pixel, palette[qPixels[bidx]], beta * .5f / saliencies[bidx], strength, x, y);
 
-				if (palette.Length > 8 && (CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor.U_Diff(pixel, c2) < (2 * acceptedDiff))) {
-					var kappa = saliencies[bidx] < .25f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
-					var c1 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
-                    c2 = BlueNoise.Diffuse(c1, palette[qPixels[bidx]], kappa, strength, x, y);
+				if (palette.Length < 3|| margin > 6) {
+					if (palette.Length > 8 && (CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor.U_Diff(pixel, c2) < (2 * acceptedDiff))) {
+						var kappa = saliencies[bidx] < .25f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
+						var c1 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
+						c2 = BlueNoise.Diffuse(c1, palette[qPixels[bidx]], kappa, strength, x, y);
+					}
 				}
+				else if (palette.Length > 8 && (CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor.U_Diff(pixel, c2) < acceptedDiff))
+					c2 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
 
 				int offset = ditherable.GetColorIndex(c2.ToArgb());
 				if (lookup[offset] == 0)
