3d41a6765e80960e3883657e6a9e17bd32ba8121
Miller Cy Chan <miller.chan@gmail.com>
2025-02-19 11:56:28 +0800

    Update GilbertCurve.cs

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -130,11 +130,11 @@ namespace nQuant.Master
 						c2 = BlueNoise.Diffuse(pixel, palette[qPixels[bidx]], beta / saliencies[bidx], strength, x, y);
 					if (palette.Length > 8 && CIELABConvertor.U_Diff(pixel, c2) > (8 * acceptedDiff)) {
 						var c1 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
-						c2 = BlueNoise.Diffuse(pixel, palette[qPixels[bidx]], beta * saliencies[bidx], strength, x, y);
+						c2 = BlueNoise.Diffuse(c1, palette[qPixels[bidx]], beta * saliencies[bidx], strength, x, y);
 					}
 				}
 
-				if (palette.Length < 3|| margin > 6) {
+				if (palette.Length < 3 || margin > 6) {
 					if (palette.Length > 8 && (CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor.U_Diff(pixel, c2) < (2 * acceptedDiff))) {
 						var kappa = saliencies[bidx] < .25f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
 						var c1 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
