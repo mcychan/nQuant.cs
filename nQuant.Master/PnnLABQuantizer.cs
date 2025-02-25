911cf69ba039a7498a85cfb50ef3b56de9e296df
Miller Cy Chan <miller.chan@gmail.com>
2025-02-25 17:27:38 +0800

    Update PnnLABQuantizer.cs

--- nQuant.Master/PnnLABQuantizer.cs
+++ nQuant.Master/PnnLABQuantizer.cs
@@ -184,7 +184,7 @@ namespace PnnQuant
 				quan_rt = -1;
 			
 			weight = Math.Min(0.9, nMaxColors * 1.0 / maxbins);
-			if (weight < .001 || (weight > .0015 && weight < .0022))
+			if (nMaxColors < 16 || weight < .001 || (weight > .0015 && weight < .0022))
 				quan_rt = 2;
 			if (weight < .04 && PG < 1 && PG >= coeffs[0, 1]) {
 				var delta = Math.Exp(1.75) * weight;
