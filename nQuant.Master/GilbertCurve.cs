2563024112c188f44278ff80c630311603832064
Miller Cy Chan <miller.chan@gmail.com>
2025-07-24 22:48:56 +0800

    Add files via upload

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -79,9 +79,11 @@ namespace nQuant.Master
 			if (palette.Length > 4) {
 				var boundary = .005 - .0000625 * palette.Length;
 				beta = (float) (weight > boundary ? Math.Max(.25, beta - palette.Length * weight) : Math.Min(1.5, beta + palette.Length * weight));
-				if(palette.Length > 32 && palette.Length < 256)
+				if (palette.Length > 32 && palette.Length < 256)
 					beta += .1f;
-			}
+                if (palette.Length >= 64 && (weight > .012 && weight < .0125) || (weight > .025 && weight < .03))
+                    beta *= 2;
+            }
 			else
 				beta *= .95f;
 
