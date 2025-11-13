23cdfdd3af4fafbd18b7fc9c5d84211dfbb60270
Miller Cy Chan <miller.chan@gmail.com>
2025-11-13 11:21:00 +0800

    Add files via upload

--- nQuant.Master/PnnLABQuantizer.cs
+++ nQuant.Master/PnnLABQuantizer.cs
@@ -274,9 +274,7 @@ namespace PnnQuant
 				heap[l] = i;
 			}
 
-			if (!isGA && quan_rt > 0 && nMaxColors < 64 && (proportional < .023 || proportional > .05) && proportional < .1)
-				ratio = Math.Min(1.0, proportional - weight * Math.Exp(2.347));
-			else if (isGA)
+			if (isGA)
 				ratio = ratioY;
 
 			/* Merge bins which increase error the least */
