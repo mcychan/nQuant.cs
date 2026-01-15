db0e03e70e3c790c7e20a16bad448a8aafe7ee5b
Miller Cy Chan <miller.chan@gmail.com>
2026-01-15 17:21:02 +0800

    Add files via upload

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -143,7 +143,7 @@ namespace nQuant.Master
 					c2 = BlueNoise.Diffuse(pixel, palette[qPixelIndex], beta / saliencies[bidx], strength, x, y);
 			}
 
-			if (margin > 6 || (palette.Length <= 32 && weight > .007)) {
+			if (margin > 6 || (palette.Length <= 32 && weight < .01 && weight > .007)) {
 				if (palette.Length > 4 && CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff)) {
 					var kappa = saliencies[bidx] < .4f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
 					var c1 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
