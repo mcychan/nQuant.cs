97d7a535a9c475475c8930ad721a111e4606d9fd
Miller Cy Chan <miller.chan@gmail.com>
2026-03-11 12:11:36 +0800

    Add files via upload

--- nQuant.Master/GilbertCurve.cs
+++ nQuant.Master/GilbertCurve.cs
@@ -227,7 +227,7 @@ namespace nQuant.Master
 			int a_pix = (int)Math.Min(Byte.MaxValue, Math.Max(error[3], 0.0));
 
 			Color c2 = Color.FromArgb(a_pix, r_pix, g_pix, b_pix);
-			if (saliencies != null && dither && !sortedByYDiff && (!m_hasAlpha || pixel.A < a_pix))
+			if (saliencies != null && dither && !sortedByYDiff && (!m_hasAlpha || Math.Abs(pixel.A - a_pix) < (.5 * margin)))
 			{
 				if (palette.Length >= 256 && saliencies[bidx] > .99f)
 					qPixels[bidx] = ditherable.DitherColorIndex(palette, c2.ToArgb(), bidx);
